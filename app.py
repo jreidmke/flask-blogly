ffa586ae5e16acba470456726dcd52d317e298e3
unknown <jreidmke@gmail.com>
2020-08-25 11:22:01 -0500

    user class created and data seeded

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Rebel4ceradio@localhost/blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+
+app.config['SECRET_KEY'] = "james"
+app.config['DEBUG_TB_INTERCEPT_DREIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def show_home():
+    users = User.query.all()
+    return render_template('home.html', users = users)
+
+@app.route('/<int:user_id>')
+def show_user_detail(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('user.html', user = user)
--- models.py
+++ models.py
@@ -1,1 +1,17 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    image_url = db.Column(db.String(200), default = 'https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/egg-3442-e1f6463624338504cd021bf23aef8441@1x.jpg')
